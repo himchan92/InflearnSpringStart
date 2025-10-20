bee480ec4b3146244de1f108cccafdc67e2129d7
himchan92 <omea92@naver.com>
2025-10-20 13:19:44 +0900

    review

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,18 +8,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //TODO: 책임, 역할 분리 등 컨트롤하는 기획자 역할
+@Configuration //설정정보관련
 public class AppConfig {
 
+    // Bean : 스프링컨테이너에 등록
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
