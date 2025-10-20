488b51736b6b5070ccd42c5ab2c25887f520164e
himchan92 <omea92@naver.com>
2025-10-20 13:03:49 +0900

    review init

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void findMember() {
+    }
+}
